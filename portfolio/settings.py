b37de5319280b0b20ef725e62ede3e5b6fc73b6a
favstats <fabio.votta@gmail.com>
2018-11-27 11:06:53 +0100

    Add more stuff

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'jobs.apps.JobsConfig',
-    'blog.apps.BlogConfig'
+    'blog.apps.BlogConfig',
+    'uploader.apps.UploaderConfig',
 ]
 
 MIDDLEWARE = [
@@ -119,8 +120,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+     os.path.join(BASE_DIR, 'portfolio/static')
+]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 MEDIA_URL = '/media/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
